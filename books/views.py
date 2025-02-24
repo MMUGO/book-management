d7ab375f4a03aec4e8d6ec9d53aeaf072013ac88
MMUGO <mmugo2020@gmail.com>
2025-02-24 23:03:46 +0300

    Update views.py

--- books/views.py
+++ books/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
-
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
 from .forms import BookForm
-
 
 def create_book(request):
     if request.method == 'POST':
@@ -36,3 +33,15 @@ def delete_book(request, pk):
         book.delete()
         return redirect('book_list')
     return render(request, 'books/delete_book.html', {'book': book})
+
+def reverse_string(s):
+    stack = list(s)
+    reversed_str = "".join(stack[::-1])
+    return reversed_str
+
+def reverse_string_view(request):
+    reversed_text = None
+    if request.method == 'POST':
+        input_text = request.POST.get('input_text', '')
+        reversed_text = reverse_string(input_text)
+    return render(request, 'books/reverse_string.html', {'reversed_text': reversed_text})
